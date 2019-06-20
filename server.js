011003b38b9cde212e4cb733d8f28ebd567a2168
Angelo Bovino <angelo.bovino@outlook.com>
2019-06-20 16:48:28 -0400

    Add CORS and dotenv support

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// app.use(express.json({ extended: true }));
+app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Setup template engine
 app.use(expressLayouts);
