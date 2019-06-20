d6060952d61e8222c84553ffb9063ab1ffed8e1c
Angelo Bovino <angelo.bovino@outlook.com>
2019-06-20 16:45:35 -0400

    Creates demo price file data and emails .csv file to user

--- routes/pricefiles.js
+++ routes/pricefiles.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const nodeMailer = require('nodeMailer');
+const fs = require('fs');
+const papa = require('papaparse');
+const createPrices = require('../utils/createPrices');
 const router = express.Router();
 
 // @Route /pricefiles
@@ -25,15 +28,15 @@ router.post('/pricefiles', (req, res) => {
   if (!userEmail) {
     errors.push({ msg: 'A user email address is required'});
   } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail)) {
-    errors.push({ msg: `${userEmail} is not a valid email address`})
+    errors.push({ msg: `${userEmail} is not a valid email address`});
   }
 
   if (!customerId) {
     errors.push({ msg: 'Customer Id is required'});
   } else if (!/^[0-9]*$/.test(customerId)) { // Regex to check if customerId string contains only numbers
-    errors.push({ msg: 'Customer Id can only contain numbers'})
+    errors.push({ msg: 'Customer Id can only contain numbers'});
   } else if (customerId.length !== 8) {
-    errors.push({ msg: 'Customer Id must be 8 digits'})
+    errors.push({ msg: 'Customer Id must be 8 digits'});
   }
   
   if (!customerFirstName) {
@@ -47,7 +50,7 @@ router.post('/pricefiles', (req, res) => {
   if (!customerEmail) {
     errors.push({ msg: 'Customer email is required'});
   } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(customerEmail)) { // check customer email format
-    errors.push({ msg: `${customerEmail} is not a valid email address`})
+    errors.push({ msg: `${customerEmail} is not a valid email address`});
   }
 
   if (!customerTitle) {
@@ -57,34 +60,54 @@ router.post('/pricefiles', (req, res) => {
   if (errors.length > 0) {
     res.render('price_file_form', {
       errors,
-      userEmail, 
-      customerId, 
-      customerFirstName, 
-      customerLastName, 
-      customerEmail, 
+      userEmail,
+      customerId,
+      customerFirstName,
+      customerLastName,
+      customerEmail,
       customerTitle
     });
   } else {
+    const date = new Date();
+    const pricesJSON = createPrices();
+    const pricesCSV = papa.unparse(pricesJSON, { header: true });
+    const fileName = `${customerId}_${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}`
+
+    fs.writeFile(`./price_files/${fileName}.csv`, pricesCSV, (err) => {
+      if (err) throw err;
+      console.log('File created');
+    });
 
     const transporter = nodeMailer.createTransport({
-      host: '',
-      port: 3000,
-      secure: false,
-      auth: null
+      service: 'outlook',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+      }
     });
 
     const mailOptions = {
-      from: 'abovino@lappusa.com',
-      to: 'angelo.bovino@outlook.com',
+      from: process.env.EMAIL,
+      to: userEmail,
       subject: `Price File for Customer ${customerId}`,
       text: `Here is your price file for customer # ${customerId}`,
+      attachments: [{
+        filename: `${fileName}.csv`,
+        path: `./price_files/${fileName}.csv`
+      }]
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log(error);
       } else {
-        res.send(`Email sent ${info.response}`);
+        console.log(info);
+        // res.send(`Email sent ${info}`);
+        res.render('price_file_form', {
+          success: {
+            msg: 'Email sent successfully'
+          }
+        })
       }
     })
   }
