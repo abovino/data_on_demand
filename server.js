1c832a55a4ff66afb4c6590bb3877a49ae02de45
Angelo Bovino <angelo.bovino@outlook.com>
2019-06-21 16:00:43 -0400

    Add express-session middleware

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Enable middlewares
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
 
 // Setup template engine
 app.use(expressLayouts);
