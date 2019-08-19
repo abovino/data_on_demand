95c85297c7dbc127273c152d38e77e423b8b9b80
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-18 22:52:50 -0400

    Moved dotenv import to top of file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
-const dotenv = require('dotenv').config();
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
